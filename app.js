e7169a792a0a6a27789940dad1b4b9313a5c6c42
hain <hainofficialweb@gmail.com>
2025-10-20 16:42:33 +0530

    chore: add health checking route

--- app.js
+++ app.js
@@ -14,10 +14,13 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, 
+    credentials: true,
   })
 );
 
+app.get("/health", (req, res) => {
+  res.status(200).send("Ok");
+});
 app.use("/", indexRouter);
 
 db.connect((err) => {
