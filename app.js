369bbaf510d6540ed827946170532116693a859f
hain <hainofficialweb@gmail.com>
2025-10-20 16:15:16 +0530

    fix: use FRONTEND_URL from .env instead of hardcoded URL in CORS

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const indexRouter = require("./router/index");
 const cors = require("cors");
-const db=require("./config/connection");
+const db = require("./config/connection");
 require("dotenv").config();
 
 const app = express();
 const PORT = 13000;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:3002", 
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // Allow credentials if needed
+    credentials: true, 
   })
-); 
+);
 
 app.use("/", indexRouter);
 
-db.connect((err)=>{
-    if(err){ 
-        console.log("Database connection failed",err);
-    }else{
-        console.log("Database connected successfully");
-    }
-})
+db.connect((err) => {
+  if (err) {
+    console.log("Database connection failed", err);
+  } else {
+    console.log("Database connected successfully");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
